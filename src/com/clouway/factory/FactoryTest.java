72f05431bea7eda80634c92e274460a7077c5ce0
KrasimirRaikov <raikov.krasimir@gmail.com>
2015-10-12 09:31:20 +0300

    initial commit

---
+++ src/com/clouway/factory/FactoryTest.java
@@ -0,0 +1,17 @@
+package com.clouway.factory;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+/**
+ * @author raikov.krasimir@gmail.com (Krasimir Raikov)
+ */
+public class FactoryTest {
+
+  @Test
+  public void additionTest(){
+    fail();
+  }
+
+}
